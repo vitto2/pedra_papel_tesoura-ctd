9ee78c6714a95cecc2d318694b29d6edbd97a00d
vitto2 <vitor2071@gmail.com>
2022-05-18 15:26:10 -0300

    Mudando a opção de empate. Nenhum dos participantes recebe ponto ao empatar.

--- ppt.js
+++ ppt.js
@@ -1,23 +1,21 @@
 
 let escolhaJogador = "Pedra";
-
 let PontoJogador = 0, PontoMaquina = 0; 
 
-let maquinaJogada = function()  { 
 
+let maquinaJogada = function()  { 
     let strs = ["Pedra", "Papel","Tesoura"]; 
     return strs[Math.floor(Math.random() * 3)]; 
 } 
 
 switch(maquinaJogada()) { 
-
+    
     case "Pedra":
         if(escolhaJogador == "Tesoura") { 
             console.log("A máquina jogou pedra!");
             PontoMaquina = PontoMaquina + 1;
             console.log("Ponto para você!")
-            console.log("A máquina tem " + PontoMaquina + " pontos.")
-            
+            console.log("A máquina tem " + PontoMaquina + " pontos.")      
         } 
         else if(escolhaJogador == "Papel") { 
             console.log("A máquina jogou pedra!");
@@ -25,10 +23,7 @@ switch(maquinaJogada()) {
             console.log("Ponto para máquina!")
             console.log("Você tem " + PontoJogador + " pontos.");
         } else { 
-            console.log("A máquina jogou pedra!");
-            PontoJogador = PontoJogador + 1; 
-            PontoMaquina = PontoMaquina + 1;
-            console.log("Vocês empataram!");
+            console.log("A máquina jogou Tesoura! Vocês empataram!");
         }; 
        break;
     case "Papel": 
@@ -43,10 +38,7 @@ switch(maquinaJogada()) {
             console.log("Ponto para máquina!")
             console.log("A máquina tem " + PontoMaquina + " pontos.")
         } else { 
-            console.log("A máquina jogou papel!");
-            PontoMaquina = PontoMaquina + 1;
-            PontoJogador = PontoJogador + 1; 
-            console.log("Vocês empataram!");
+            console.log("A máquina jogou Tesoura! Vocês empataram!");
         }
         break;
     case "Tesoura": 
@@ -61,10 +53,7 @@ switch(maquinaJogada()) {
             console.log("Ponto para máquina!")
             console.log("A máquina tem " + PontoMaquina + " pontos.")
         } else { 
-            PontoMaquina = PontoMaquina + 1;
-            PontoJogador = PontoJogador + 1; 
-            console.log("A máquina jogou Tesoura!");
-            console.log("Vocês empataram!");
+            console.log("A máquina jogou Tesoura! Vocês empataram!");    
         }
         break; 
 
